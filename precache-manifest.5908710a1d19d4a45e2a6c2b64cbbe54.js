6dd71371576e92253e87158d0b4f16cc6a342a6c
fabiankaegy <fabian@arvernus.info>
2018-11-03 22:30:16 +0100

    Updates

---
+++ precache-manifest.5908710a1d19d4a45e2a6c2b64cbbe54.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "333b23a6e0c229630e61",
+    "url": "/static/js/main.333b23a6.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "333b23a6e0c229630e61",
+    "url": "/static/css/main.c1df3711.chunk.css"
+  },
+  {
+    "revision": "2f5eb1787181dc6f84dea05dc41036f1",
+    "url": "/index.html"
+  }
+];
